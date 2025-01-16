de8a097111068dc035992ff9f58d94aa6bcedbb1
Raju <drzraju@gamil.com>
2025-01-16 12:49:22 +0600

    change setting

--- mark/mark/settings.py
+++ mark/mark/settings.py
@@ -118,12 +118,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = BASE_DIR / 'media'  
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
